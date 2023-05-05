075555e6e3329c06a0bee80ad6d6dbc63715064c
flioner <lionerrochae@gmail.com>
2023-05-04 23:21:13 -0600

    removed unnecesary imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,6 @@
 import Layout from "../components/layout/layout";
-import { ProfileSlider, VerticalSlider } from "../components/ui/slider/slider";
 import s from "../styles/Home.module.css";
 import React, { useState } from "react";
-import Typewriter from "typewriter-effect";
-import { LaptopViewer } from "../components/threejs/canvas";
-import { VantaBG } from "../components/threejs/vanta";
-import { ObjViewer } from "../components/threejs/threejs";
-import { Modal } from "../components/ui/modal/modal";
-import { ImageCollection } from "../components/ui/image_collection/image_collection";
-import { ImageHero } from "../components/ui/imgHero/imghero";
-import DotWave from "../components/threejs/dots/dotwave";
-import { ProductDemo } from "../components/product_demo/productDemo";
-import Categories from "../components/categories/categories";
-import { Contact } from "../components/ui/contact/contact";
 
 export default function Home() {
   const [visibleDot, setDots] = useState(false);
